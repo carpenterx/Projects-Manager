812715d633c01da2db4e65033f6bec0277fae3dc
Sorin <jordache_sor@yahoo.com>
2021-06-15 15:28:59 +0300

    added repo info class
    fixes #14

---
+++ Projects Manager/Models/RepoInfo.cs
@@ -0,0 +1,16 @@
+﻿namespace Projects_Manager.Models
+{
+    class RepoInfo
+    {
+        public Repo Repo { get; set; }
+        public string Notes { get; set; }
+        public bool IsHidden { get; set; }
+
+        public RepoInfo(Repo repo, bool isHidden = false)
+        {
+            Repo = repo;
+            Notes = "";
+            IsHidden = isHidden;
+        }
+    }
+}
