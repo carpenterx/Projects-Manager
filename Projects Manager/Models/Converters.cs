2207f8e3c3bf32775fd0ffa65bb2d9ecff76fb85
Sorin <jordache_sor@yahoo.com>
2021-06-17 15:20:04 +0300

    humanized project names
    fixes #37

--- Projects Manager/Models/Converters.cs
+++ Projects Manager/Models/Converters.cs
@@ -46,4 +46,18 @@ namespace Projects_Manager.Models
             throw new NotImplementedException();
         }
     }
+
+    public class HumanizedNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string name = (string)value;
+            return name.Replace('-', ' ');
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
--- Projects Manager/Properties/Settings.Designer.cs
+++ Projects Manager/Properties/Settings.Designer.cs
@@ -49,7 +49,7 @@ namespace Projects_Manager.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
         public bool UseLocalData {
             get {
                 return ((bool)(this["UseLocalData"]));
