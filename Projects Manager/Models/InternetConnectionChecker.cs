e87305a61d5c86c148cd7db556f97b82ef1e3e06
Sorin <jordache_sor@yahoo.com>
2021-06-23 14:43:32 +0300

    added connection checker
    fixes #40

---
+++ Projects Manager/Models/InternetConnectionChecker.cs
@@ -0,0 +1,19 @@
+﻿using NETWORKLIST;
+
+namespace Projects_Manager.Models
+{
+    public class InternetConnectionChecker
+    {
+        private readonly INetworkListManager networkListManager;
+
+        public InternetConnectionChecker()
+        {
+            networkListManager = new NetworkListManager();
+        }
+
+        public bool IsConnected()
+        {
+            return networkListManager.IsConnectedToInternet;
+        }
+    }
+}
